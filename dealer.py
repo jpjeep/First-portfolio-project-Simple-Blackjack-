c3b419b5e804db752d01f12ea2ede3077da91946
jpjeep <johnpowellcosido04@gmail.com>
2026-01-05 23:50:56 +0800

    Update dealer.py

--- dealer.py
+++ dealer.py
@@ -1,9 +1,6 @@
 from hand import Hand
-#only hold state
-#like the hand and money available
 class Dealer:
     def __init__(self, hand):
-        #this is an object turned attribute to be handled in the main function
         self.hand = hand
     @property
     def score(self):
@@ -18,7 +15,4 @@ class Dealer:
             pass
         print (f"\nThe dealer's cards are...")
         for a in self.hand:
-            print (f"{a.rank} of {a.suit}.")
-        #determines if 17 or not
-        #if 17 cannot draw anymore
-        #elif 17 draw to determine winner   
+            print (f"{a.rank} of {a.suit}.") 
